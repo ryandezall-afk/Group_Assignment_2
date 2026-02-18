8c80daca0cf6a12bf904a06a110b12d2418ccc73
ryandezall <ryandezall@the-black-pearl.lan>
2026-02-17 21:16:25 -0700

    sedan class made

---
+++ src/problemdomain/Sedan.java
@@ -0,0 +1,25 @@
+package problemdomain;
+
+public class Sedan extends Vehicle {
+	private String trunkSize;
+	
+	public Sedan(long carID, String vehicleType, String subType, int speed, double fuel, 
+			int seats, int year, String drivetrain, int price, int quantity, String trunkSize) {
+		super(carID, vehicleType, subType, speed, fuel, seats, year, drivetrain, price, quantity);
+		this.trunkSize = trunkSize;
+	}
+	
+	public String getTrunkLabelSize() {
+		switch(trunkSize.toUpperCase()) {
+		case "L": return "Large/spacious trunk";
+		case "S": return "Small trunk";
+		case "M": return "Moderate trunk";
+		default: return "Unknown";
+		}
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nTrunk Size: " + getTrunkLabelSize();
+	}
+}
